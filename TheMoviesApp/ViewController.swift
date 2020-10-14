6644de01a65473a1be9239c02e84c86655c40f73
Oliver Perez <oliverperez.e@gmail.com>
2020-10-14 00:03:31 -0500

    Add base project

---
+++ TheMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMoviesApp
+//
+//  Created by Oliver Jordy Pérez Escamilla on 14/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
